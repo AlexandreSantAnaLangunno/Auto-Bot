58bb017c11e82453135d821e3d4500b4b1187a7c
Alexandre <71261076+ajeliquesplays@users.noreply.github.com>
2020-11-04 10:31:04 -0300

    Pre-Definições Bot-Canva

--- Bot-Canva.py
+++ Bot-Canva.py
@@ -1,9 +1,26 @@
 
+# Olá Mundo para dar tudo certo \(•ω•`\)
+
 print("Olá Mundo")
 
 
+# Entrar no Link do Canva
+
+
+# Duplicar uma pagina no Canva
+
+
+# Pegar as informações da Area de Transferencia
+
+
+# Colar em Respectivos Lugares na Pagina
+    
+    ## Lugares = (Area_de_Texto, Area_de_Bibliografia)
+
+# (Opsional) Editar e Ajustar as Informações 
 
 Ola_Mundo = True
+
 
 if Ola_Mundo == True:
     pass
