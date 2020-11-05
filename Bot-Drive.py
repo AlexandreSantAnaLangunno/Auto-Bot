2f6ab0c98c40ffbed83adc46534bca19e110c7e7
Alexandre <71261076+ajeliquesplays@users.noreply.github.com>
2020-11-05 18:06:19 -0300

    Primeiros Passos
    Coloquei mais bibliotecas e também  dei os primeiros passos 
    =)

--- Bot-Drive.py
+++ Bot-Drive.py
@@ -1,4 +1,16 @@
+# Importando Bibliotecas
+
 import selenium
+
+import time
+
+import webdriver_manager
+
+from webdriver_manager.chrome import ChromeDriverManager
+
+from selenium import webdriver
+
+from time import sleep
 
 # Olá Mundo para dar tudo certo \(•ω•`\)
 
@@ -7,9 +19,17 @@ print("Olá Mundo")
 # pegar as informações do site do Google Drive e coloca-las em classes ou listas
 
 
+
     ## Informações = (Link)
+### Fazendo a Função que abra o Site
 
+driver = webdriver.Chrome(ChromeDriverManager().install())  
 
+driver.get('https://docs.google.com/document/d/1AD_09SmLzTRvJ-6kWg1kTs_0BNyt0bmWv4yONgO9vrs/edit?usp=sharing') ####### Aqui irei chamar a Função
+
+#### Prescissamos de Deijar um Tempo para que o Programa Não Feche
+
+time.sleep(1000) ###### 1000 == ( '10 segundos' )
 
 # pegar as ações que devo executar no Google Drive
 
