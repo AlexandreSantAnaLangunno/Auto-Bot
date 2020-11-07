d5bc63a1ace41bb0dea3f653ad7840518674aa13
Alexandre <71261076+ajeliquesplays@users.noreply.github.com>
2020-11-07 08:06:20 -0300

    Update das Bibliotecas e Add Link
    Adicionei mais Bibliotecas e adicionei o link que irei trabalhar

--- Bot-Canva.py
+++ Bot-Canva.py
@@ -1,5 +1,18 @@
+# Importando Bibliotecas
+
+import webdriver_manager
+
+import time
+
 import selenium
+
 import os
+
+from webdriver_manager.chrome import ChromeDriverManager
+
+from selenium import webdriver
+
+from time import sleep
 
 # Olá Mundo para dar tudo certo \(•ω•`\)
 
@@ -8,6 +21,11 @@ print("Olá Mundo")
 
 # Entrar no Link do Canva
 
+driver = webdriver.Chrome(ChromeDriverManager().install())  
+
+driver.get('https://www.canva.com') # exemplo o link que irei entrar é outro 
+
+time.sleep(1000)
 
 # Duplicar uma pagina no Canva
 
