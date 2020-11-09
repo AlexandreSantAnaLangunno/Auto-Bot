adddea387f64122eef0f45a5389dba5c7a71ffd5
Alexandre <71261076+ajeliquesplays@users.noreply.github.com>
2020-11-09 08:05:39 -0300

    Add o Projeto da interface Gráfica
    Organizei a interface grafica para ser mas facil programar
    Fiz Um Desenho no Paint para eu ter noção do que fazer =)

---
+++ Boas_Vindas.py
@@ -0,0 +1,59 @@
+
+# Ola Mundo Para Dar Certo \( * ^0^ * )/
+
+olamundo = {print ("Olá Mundo! "), True}
+# Importando Bibliotecas
+
+import os
+
+import PySimpleGUI
+   
+    ## Essas Será as Bibliotecas
+
+
+# Criar Tela
+
+    ## Funções Iniciais para Uma Tela
+
+        ### Definir Requisitos da Tela
+
+        ### Definir Função ("Desenhar e Atualizar")
+
+        ### Chamar as Funções ("Desenhar e Atualizar")
+
+
+    # Definir o Botão ("Começar")
+
+        ## O Arquivo que ele ira Abrir \\ ( "Bot-Drive" )
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+## Olá Mundo Concluido
+if olamundo == ( True ):
+    print ("Olá Mundo Concluido")
+
+    ### Fim do Olá Mundo 
