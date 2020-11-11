ea4726781d150c1742cda0fcf278b8aab5b933be
Alexandre <71261076+ajeliquesplays@users.noreply.github.com>
2020-11-11 07:28:29 -0300

    Definindo a Tela de Boas-Vindas

--- Boas_Vindas.py
+++ Boas_Vindas.py
@@ -6,7 +6,7 @@ olamundo = {print ("Olá Mundo! "), True}
 
 import os
 
-import PySimpleGUI
+import PySimpleGUI as Pysg
    
     ## Essas Será as Bibliotecas
 
@@ -16,10 +16,39 @@ import PySimpleGUI
     ## Funções Iniciais para Uma Tela
 
         ### Definir Requisitos da Tela
+    
+    #### Descobri que pode colocar temas =)
 
-        ### Definir Função ("Desenhar e Atualizar")
+Pysg.theme('LightBlue') 
+    ##### coloquei o 'Azul Claro' PorQue Combina com o 'Canva'
+
+requisito = [ [ Pysg.Text("Auto-Bot" ) ],
+    
+              [ Pysg.Text("Olá Ben-Vindo de Volta " )  ],
+              
+              [Pysg.Text( " Obs. Colocar o Ano Abreviado: ' xx/xx/yy ' ", text_color = 'red') ],
+
+              [ Pysg.Text('Data 1: ') ], [ Pysg.Input(key='-INPUT-', size = ( 8, 4 )) ],      
+              
+              [ Pysg.Text('Data 2: ') ], [ Pysg.Input(key='-INPUT-', size = ( 8, 4 )) ],
+              
+              [ Pysg.Button( 'Começar ' ) ] ]             
+       
+### Definir Função ("Desenhar e Atualizar")
+                    
+                    # Titulo   
+janela = Pysg.Window('Auto-Bot', requisito)
 
         ### Chamar as Funções ("Desenhar e Atualizar")
+while True:
+    
+    desenhar, atualizar = janela.read()
+    
+    if desenhar == Pysg.WINDOW_CLOSED or desenhar == 'Quit':
+        break
+    
+    
+
 
 
     # Definir o Botão ("Começar")
