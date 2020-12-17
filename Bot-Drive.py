8684eee34436817e73a8fbda69e925d615392654
Alexandre <71261076+AlexandreSantAnaLangunno@users.noreply.github.com>
2020-12-16 21:11:55 -0300

    Upgrade Bot Drive
    Add  Ligação da Interface Gráfica com a Lista que usarei para fazer a Automação
    Tentando procurar o Elemento de data com o Drive.

--- Bot-Drive.py
+++ Bot-Drive.py
@@ -1,10 +1,11 @@
 # Importando Bibliotecas
-
 import selenium
 
 import time
 
 import webdriver_manager
+
+import PySimpleGUI as Pysg
 
 from webdriver_manager.chrome import ChromeDriverManager
 
@@ -12,45 +13,56 @@ from selenium import webdriver
 
 from time import sleep
 
-# Olá Mundo para dar tudo certo \(•ω•`\)
-
+# Olá Mundo para dar tudo certo \(•ω• \)
 print("Olá Mundo")
 
-# pegar as informações do site do Google Drive e coloca-las em classes ou listas
+def Comecar():
+
+    data01 = str(atualizar['Data01'])
+    data02 = str(atualizar['Data02'])
+
+    driver = webdriver.Chrome(ChromeDriverManager().install())  
+
+    driver.get('https://docs.google.com/document/d/1AD_09SmLzTRvJ-6kWg1kTs_0BNyt0bmWv4yONgO9vrs/edit?usp=sharing')
+
+    time.sleep(20) 
+
+    Find_Data = driver.find_elements_by_link_text(data01)
+
+    Find_Data.click()
+    Find_Data.click()
+    Find_Data.click()
 
 
+Pysg.theme('LightBlue')
+tela = [
+    # Texto Inicial
+    [Pysg.Text('Bot-Canva', font=('Bold'))], 
+    [Pysg.Text('Bem-vindo de volta!', text_color='blue')],
+    # Aviso De Formatação 
+    [Pysg.Text('Por Favor Colocar a data separada por "/" : ("01/01/2020")', text_color ='red', size=(25,2))],
+    # Campos de Input
+    [Pysg.Text("Data 1: "), Pysg.Input(size=(38,20), key ='Data01')],
+    [Pysg.Text("Data 2: "), Pysg.Input(size=(38,20), key ='Data02')],
+    # Começa a Automação com Selenium
+    [Pysg.Button('Começar', size = (40,1))]
+    ]
 
-    ## Informações = (Link)
-### Fazendo a Função que abra o Site
+# Definir a Janela
+janela = Pysg.Window( 'Auto-Bot', tela )
 
-driver = webdriver.Chrome(ChromeDriverManager().install())  
+# Loop de Janela
+while True:
+    desenhar, atualizar = janela.read()
 
-driver.get('https://docs.google.com/document/d/1AD_09SmLzTRvJ-6kWg1kTs_0BNyt0bmWv4yONgO9vrs/edit?usp=sharing')
+    if desenhar == Pysg.WINDOW_CLOSED or desenhar == 'Quit':
+        break
 
-#### Prescissamos de Deixar um Tempo para que o Programa Não Feche
-
-time.sleep(1000) ###### 1000 == ( '10 segundos' )
-
-# pegar as ações que devo executar no Google Drive
-
-
-    ## ações = (Rolar, Achar_o_Texto_não_formatado)
-
-### Toda data que eu coloquei na tabela do Google Docs termina com ("&zwnj")
-    
-
-
-# Colocar as informações na Área de Transferencia ( Ou Crlt + C ) 
-    
-    
-    ## informações = (Textos, Bibliografia do Autor)
-
-
-
+    if desenhar == 'Começar':
+        Comecar()
+        
+# Olá Mundo Concluido com Sucesso
 Ola_Mundo = True
-
-
 if Ola_Mundo == True:
-    pass
     print (" Obrigado ;) ")
 
